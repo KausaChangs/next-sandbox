d6120b2fb71bb9cfcee1165c9acc011b737cf930
Kausa Changaya <changayakausa@gmail.com>
2024-06-18 12:24:44 +0200

    created about route that navigates to about page
    created about route that navigates to about page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/team">Team</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
